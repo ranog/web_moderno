26b90845c65007fbf9856a86c8350811bfc660a6
ranog <jprnogueira@yahoo.com.br>
2019-01-20 19:18:53 -0200

    58. Usando a Estrutura DO/WHILE

---
+++ exercicios-js/controle/do_while.js
@@ -0,0 +1,14 @@
+function aleatorio(min, max){
+   const valor = Math.random() * (max - min) + min
+   return Math.floor(valor)
+}
+
+let opcao = -1
+
+do{
+   opcao = aleatorio(-1, 10)
+   console.log(`Opção escolhida foi ${opcao}.`)
+} while(opcao != -1)
+
+console.log('Até a próxima!')
+
